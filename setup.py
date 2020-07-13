9bab528666e42cc0c89da2887900c4b3f0c49f34
Simone <simo.pc95@gmail.com>
2020-07-13 20:34:36 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="host-checker",
-    version="1.0.0",
+    version="1.0.1",
     description="Simple CLI tool to check who is hosting a website",
     long_description=long_description,
     long_description_content_type="text/markdown",
