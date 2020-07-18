f38a5580948053fdac785db878cc7a0edcc1c9bf
Simone <simo.pc95@gmail.com>
2020-07-18 12:05:58 +0200

    Added source under src folder

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SimoneCorazza/HostChecker",
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
